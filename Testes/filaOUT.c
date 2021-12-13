e33f2e3b8098d038f593c17f49d57f06ff61b68a
itallo <itallopss@dcomp.ufs.br>
2021-12-13 16:17:51 +0000

    testes com fila prod/cons

--- Testes/filaOUT.c
+++ Testes/filaOUT.c
@@ -10,186 +10,129 @@
  * Luan Melo Guimarães
  * Lucas Carvalho Gomes Santiago 
  * 
- * Compilação: mpicc -lpthread -o filaOUT filaOUT.c
- * Execução:   mpiexec -n 3 ./filaOUT
+ * Compilação: gcc -g -Wall -o filaOUT filaOUT.c -lpthread -lrt
+ * Execução:   ./filaOUT
  * 
  * 
  * TODO: > função snapshot
  * 
+ * testes de produtor-consumidor usando clocks 
  * 
  */
  
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <pthread.h>
-#include <string.h>  
 #include <unistd.h>
 #include <time.h>
-#include <mpi.h>  
+#include <semaphore.h>
 
-
-#define THREAD_NUM 2
-
+#define THREAD_NUM 3
+#define MAX 10
 
 typedef struct Clock { 
    int p[3];
 } Clock;
 
-int buffer[10];
+
+sem_t semEmpty;
+sem_t semFull;
+
+pthread_mutex_t mutexBuffer;
+
+Clock buffer[10];
 int count = 0;
 
-void Event(int pid, Clock *clock){
-   clock->p[pid]++;   
-   printf("[E] Process: %d, Clock: (%d, %d, %d)\n", pid, clock->p[0], clock->p[1], clock->p[2]);
+void* producer(void* args) {
+
+    while (1) {
+        // Produce
+        int x = rand() % 100;
+        
+        Clock clock = {{x, x, x}};
+        sleep(1);
+
+        // Add to the buffer
+        sem_wait(&semEmpty);
+        pthread_mutex_lock(&mutexBuffer);
+        
+        buffer[count] = clock;
+        
+        count++;
+        pthread_mutex_unlock(&mutexBuffer);
+        sem_post(&semFull);
+    }
 }
 
+void* consumer(void* args) {
 
+    while (1) {
+        int x;
+        int y;
+        int z;
+        
+        // Remove from the buffer
+        sem_wait(&semFull);
+        pthread_mutex_lock(&mutexBuffer);
+        
+        x = buffer[0].p[0];
+        y = buffer[0].p[1];
+        z = buffer[0].p[2];
+        count--;
+        
+        pthread_mutex_unlock(&mutexBuffer);
+        sem_post(&semEmpty);
 
-
-void Send(int pidS, int pidR, Clock *clock){
-   clock->p[pidS]++;
-   
-   int array[3] = {0, 0, 0};
-   
-   for(int i = 0; i < 3; i++){
-      array[i] = clock->p[i];
-   }
-   
-   MPI_Send(array ,3 , MPI_INT, pidR, 1, MPI_COMM_WORLD);
-   printf("[S] Process: %d To: %d, Clock (%d, %d, %d)\n" ,pidS, pidR, clock->p[0], clock->p[1], clock->p[2]);
-   // TO DO
-   
+        // Consume
+        printf("Got (%d, %d, %d)\n", x, y, z);
+        sleep(1);
+    }
 }
 
-void Receive(int pidS ,int pidR, Clock *clock){
-   clock->p[pidR]++;
-   
-   int arrayR[3];
-   MPI_Recv(arrayR, 3, MPI_INT, pidS, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-   
-   for (int i = 0; i < 3; i++) 
-	   if (arrayR[i] > clock->p[i])
-		   clock->p[i] = arrayR[i];
-   
-   
-   printf("[R] Process: %d From: %d, Clock (%d, %d, %d)\n" ,pidR, pidS, clock->p[0], clock->p[1], clock->p[2]);
-   // TO DO
-
-}
-
-
-
-void *producer (void* args){
-   
-   int x = rand(); 
-   
-   buffer[count] = x;
-   count ++;
-   
-}
-
-void *consumer (void* args){
-   
-   int y = buffer[count - 1];
-   count--;
-   
-   printf("Got %d\n", y);
-}
-
-
-void threads() {
+/*void thread(Fila fila){
    pthread_t th[THREAD_NUM];
+   
    int i;
-   for(i=0; i < THREAD_NUM ; i++){
-      if(i % 2 == 0){
-         if(pthread_create(&th[i], NULL, &producer, NULL) != 0){
-            perror("Falha ao criar thread");
-         }
-      } else {
-         if(pthread_create(&th[i],NULL, &consumer, NULL) != 0){
-            perror("Falha ao criar thread");
-         }
-      }
-   }
-   for(i=0 ; i < THREAD_NUM; i++) {
-      if(pthread_join(th[i],NULL) != 0){
-         perror("Falha ao dar join na thread");
-      }
-   }
-   
+    for (i = 0; i < THREAD_NUM; i++) {
+        if (i > 0) {
+            if (pthread_create(&th[i], NULL, &producer, (void*) &fila) != 0) {
+                perror("Failed to create thread");
+            }
+        } else {
+            if (pthread_create(&th[i], NULL, &consumer, (void*) &fila) != 0) {
+                perror("Failed to create thread");
+            }
+        }
+    }
+    for (i = 0; i < THREAD_NUM; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            perror("Failed to join thread");
+        }
+    }
 }
+*/
 
-
-
-
-// Representa o processo de rank 0
-void process0(){
+int main(int argc, char* argv[]) {
+    srand(time(NULL));
+    
+    pthread_mutex_init(&mutexBuffer, NULL);
+    sem_init(&semEmpty, 0, 10);
+    sem_init(&semFull, 0, 0);
+    
+    //thread(&fila1);
+    pthread_t th1;
+    pthread_t th2;
+    
    
-   srand(time(NULL));
-   
-   
-   threads(); // cria as 3 threads 
-   
-   Clock clock = {{0,0,0}};
-  // printf("[ ] Process: %d, Clock: (%d, %d, %d)\n", 0, clock.p[0], clock.p[1], clock.p[2]);
-   
-   
-   /*
-   Event(0, &clock); // operacao a
-   Send(0, 1, &clock); //operacao b
-   Receive(1, 0, &clock);// operacao c
-   Send(0, 2, &clock); // operacao d 
-   Receive(2, 0, &clock); // operacao e
-   Send(0, 1, &clock); // operacao f 
-   Event(0, &clock); // operacao g
-   */
+    pthread_create(&th1, NULL, &producer, NULL);
+    pthread_create(&th2, NULL, &producer,  NULL);
+    pthread_join(th1, NULL); 
+    pthread_join(th2, NULL);
+    
+    
+    sem_destroy(&semEmpty);
+    sem_destroy(&semFull);
+    pthread_mutex_destroy(&mutexBuffer);
+    return 0;
 }
-
-// Representa o processo de rank 1
-void process1(){
-   Clock clock = {{0,0,0}};
- //  printf("[ ] Process: %d, Clock: (%d, %d, %d)\n", 1, clock.p[0], clock.p[1], clock.p[2]);
-   
-   /*
-   Send(1, 0, &clock); // operacao h
-   Receive(0, 1, &clock); // operacao i
-   Receive(0, 1, &clock); // operacao j
-   // TO DO
-   */
-}
-
-// Representa o processo de rank 2
-void process2(){
-   Clock clock = {{0,0,0}};
-  // printf("[ ] Process: %d, Clock: (%d, %d, %d)\n", 2, clock.p[0], clock.p[1], clock.p[2]);
-   /*
-   Event(2, &clock); // operacao k
-   Send(2, 0, &clock); // operacao l
-   Receive(0, 2, &clock); // operacao m
-
-   // TO DO
-   */
-}
-
-int main(void) {
-   
-   int my_rank;               
-
-   MPI_Init(NULL, NULL); 
-   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
-
-   if (my_rank == 0) { 
-      process0();
-   } else if (my_rank == 1) {  
-      process1();
-   } else if (my_rank == 2) {  
-      process2();
-   }
-
-   /* Finaliza MPI */
-   MPI_Finalize(); 
-
-   return 0;
-}  /* main */
-
-
